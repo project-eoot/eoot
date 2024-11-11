141cc287631dc6e991d6fa141c321497f3e8ed26
ehfql6363 <ehfql6363@naver.com>
2024-11-11 12:23:09 +0900

    remove: .gradle, build, .idea 삭제 .gradle, build, .idea 삭제 후 모든 파일 재 push #2

---
+++ src/main/java/com/ssafy/eoot/config/CORSConfig.java
@@ -0,0 +1,8 @@
+package com.ssafy.eoot.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CORSConfig {
+}
