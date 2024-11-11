a83fe7ca9c4816b4280ad1c1cc9e4c3f297a0daf
ehfql6363 <ehfql6363@naver.com>
2024-11-11 16:14:18 +0900

    feat: 로그인 시 필요한 URI 정의 소셜 로그인, 인증 요청, 토큰 URI 관련 클래스 구현 None
    Signed-off-by: ehfql6363 <ehfql6363@naver.com>

---
+++ src/main/java/com/ssafy/eoot/config/AuthorizationProperties.java
@@ -0,0 +1,30 @@
+package com.ssafy.eoot.config;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+
+@ConfigurationProperties("authorization")
+@Getter
+@RequiredArgsConstructor
+public class AuthorizationProperties {
+
+    // 소셜 로그인 처리 URI
+    private final String authorizationBaseUri;
+
+    // 인증 요청 처리 URI
+    private final String redirectionBaseUri;
+
+    private final String tokenUri;
+    private final List<String> redirectUriParameterAllowedRegistrations;
+
+    public String getAuthorizationUri(){
+        return authorizationBaseUri + "/*";
+    }
+
+    public String getRedirectionUri(){
+        return redirectionBaseUri + "/*";
+    }
+}
