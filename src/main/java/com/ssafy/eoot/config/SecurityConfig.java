b18a7525f41972ceef8d135edaece4f62539e679
ehfql6363 <ehfql6363@naver.com>
2024-11-13 17:21:05 +0900

    feat: Security Config 설정 Security 설정 개발 중 None
    Signed-off-by: ehfql6363 <ehfql6363@naver.com>

---
+++ src/main/java/com/ssafy/eoot/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.ssafy.eoot.config;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+}
